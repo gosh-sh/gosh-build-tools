3a66ead1cfd75d150d671f66b549613c8e1bf81c
SilkovAlexander <slexander.s@tonlabs.io>
2023-04-28 17:12:05 +0300

    Create dir for config

--- gosh/src/config/mod.rs
+++ gosh/src/config/mod.rs
@@ -86,6 +86,12 @@ impl Config {
             env::var("GOSH_CONFIG_PATH").unwrap_or_else(|_| defaults::CONFIG_LOCATION.to_string());
         let config_path = shellexpand::tilde(&config_path).into_owned();
         let config_path = Path::new(&config_path);
+        match config_path.parent() {
+            Some(parent_path) => {
+                std::fs::create_dir_all(parent_path)?;
+            }
+            None => {}
+        }
         let mut file = std::fs::File::create(config_path)?;
         file.write_all(serde_json::to_string_pretty(&self)?.as_bytes())?;
         Ok(())
