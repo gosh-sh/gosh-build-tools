f981bbe90260d322dd874a4411608abafc173f18
sostrovskyi <sostrovskyi>
2023-05-24 13:54:23 +0300

    gosh tools build

--- .ci/scripts/update-github-release.sh
+++ .ci/scripts/update-github-release.sh
@@ -9,7 +9,7 @@ VERSION=$2
 BRANCH=$3
 
 RELEASE_ID=$(curl -sH "Authorization: token ${TOKEN}" \
-    "https://api.github.com/repos/gosh-sh/gosh/releases/tags/${VERSION}" | jq -r '.id' | cut -d'{' -f1)
+    "https://api.github.com/repos/gosh-sh/gosh-build-tools/releases/tags/${VERSION}" | jq -r '.id' | cut -d'{' -f1)
 
 curl -X PATCH -H "Accept: application/vnd.github+json" -H "Authorization: token ${TOKEN}" \
     https://api.github.com/repos/gosh-sh/gosh/releases/"$RELEASE_ID" \
